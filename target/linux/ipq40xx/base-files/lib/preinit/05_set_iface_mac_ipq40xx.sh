1b4d0dbcaa6c4823a3cd7173369e050c8b2a0e05
dongjuanyong <38952488+dongjuanyong@users.noreply.github.com>
2019-04-06 23:12:56 +0800

    fifth try

--- target/linux/ipq40xx/base-files/lib/preinit/05_set_iface_mac_ipq40xx.sh
+++ target/linux/ipq40xx/base-files/lib/preinit/05_set_iface_mac_ipq40xx.sh
@@ -9,6 +9,12 @@ preinit_set_mac_address() {
 		ip link set dev eth0 address $(macaddr_add "$base_mac" +1)
 		ip link set dev eth1 address $(macaddr_add "$base_mac" +3)
 		;;
+	asus,rt-acrh17|\
+	asus,rt-ac58u)
+		CI_UBIPART=UBI_DEV
+		mac=$(macaddr_add $(mtd_get_mac_binary_ubi Factory 4102) +1)
+		ifconfig eth0 hw ether $mac 2>/dev/null
+		;;
 	meraki,mr33)
 		mac_lan=$(get_mac_binary "/sys/bus/i2c/devices/0-0050/eeprom" 102)
 		[ -n "$mac_lan" ] && ip link set dev eth0 address "$mac_lan"
