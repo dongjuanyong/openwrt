59bc99228a13adb3e9d550a57dd7cc8d703904cd
dongjuanyong <38952488+dongjuanyong@users.noreply.github.com>
2020-09-02 19:46:28 +0800

    reslove confliction

--- include/target.mk
+++ include/target.mk
@@ -14,7 +14,7 @@ DEVICE_TYPE?=router
 
 # Default packages - the really basic set
 DEFAULT_PACKAGES:=\
-block-mount coremark kmod-nf-nathelper kmod-nf-nathelper-extra kmod-ipt-raw wget libustream-openssl ca-certificates \
+block-mount coremark kmod-nf-nathelper kmod-nf-nathelper-extra kmod-ipt-raw wget ca-certificates \
 default-settings luci luci-app-ddns luci-app-upnp luci-app-autoreboot luci-app-webadmin \
 luci-app-filetransfer luci-app-vsftpd luci-app-ssr-plus luci-app-unblockmusic \
 luci-app-arpbind luci-app-vlmcsd luci-app-wol luci-app-ramfree \
