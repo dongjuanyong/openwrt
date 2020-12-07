612870d3bff170ae3005a3a6d33eaac7d018c80b
dongjuanyong <38952488+dongjuanyong@users.noreply.github.com>
2020-12-07 21:43:03 +0800

    Update v2raypro.lua

--- package/lean/luci-app-v2ray-pro/luasrc/model/cbi/v2raypro.lua
+++ package/lean/luci-app-v2ray-pro/luasrc/model/cbi/v2raypro.lua
@@ -219,8 +219,8 @@ servername.datatype = "host"
 servername:depends("tls", "1")
 
 xtls = s:taboption("main",Flag, "xtls", translate("Using XTLS"))
-xtls:depends({protocol="vless", tls="1", network_type="tcp"})
-xtls:depends({protocol="vless", tls="1", network_type="kcp"})
+xtls:depends({use_xray="1", protocol="vless", tls="1", network_type="tcp"})
+xtls:depends({use_xray="1", protocol="vless", tls="1", network_type="kcp"})
 
 mux = s:taboption("main",Flag, "mux", translate("Mux"))
 mux.rmempty = false
